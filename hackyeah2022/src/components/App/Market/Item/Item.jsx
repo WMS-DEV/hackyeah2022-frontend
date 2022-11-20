1e0eba6f391709b4aa12d2e372c4d3b3200f6f75
Kisiekx <43110486+Kisiekx@users.noreply.github.com>
2022-11-20 09:30:30 +0100

    login and register update

--- hackyeah2022/src/components/App/Market/Item/Item.jsx
+++ hackyeah2022/src/components/App/Market/Item/Item.jsx
@@ -1,84 +1,70 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link} from "react-router-dom";
 import './Item.css'
 import {Footer} from "../../Footer/Footer";
+import {useAuth} from "../../../Authentication/AuthProvider";
+import '../../../../index.css'
 
-export const Item = (/*props*/) => {
+export const Item = () => {
 
-    const props = {
-        "auction": {
-        "id": 0,
-            "userId": 0,
-            "buyerId": 0,
-            "title": "string",
-            "location": "string",
-            "auctionPictureLink": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png",
-            "description": "string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string string ",
-            "datePosted": "2022-11-20T05:17:08.287Z",
-            "dateSold": "2022-11-20T05:17:08.287Z",
-            "category": "AUTOMOTIVE",
-            "status": "LISTED",
-            "listedFor": "MARKET",
-            "auctionPrice": 0,
-            "selectedShippingType": "PACKAGE_MACHINE"
-    },
-        "seller": {
-        "id": 0,
-            "isAdmin": true,
-            "lastName": "string",
-            "username": "string",
-            "phoneNumber": "string",
-            "city": "string",
-            "street": "string",
-            "houseNumber": "string",
-            "flatNumber": "string",
-            "packageMachineNumber": "string",
-            "postalCode": "string",
-            "additionalShippingInformation": "string",
-            "accountType": "REGULAR_USER",
-            "firstName": "string",
-            "voivodeship": "string",
-            "pictureUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png",
-            "enabled": true,
-            "credentialsNonExpired": true,
-            "formattedAddress": "string",
-            "accountNonExpired": true,
-            "accountNonLocked": true
-    }
-    }
+    const {apiLink, token} = useAuth()
+    const [auctionId, setAuctionId] = useState(window.location.href.match(/id=([^&]*)/)[1]);
+    const [auction, setAuction] = useState();
+
+    useEffect(() => {
+        const fetchAuction = async () => {
+            const data = await fetch(`${apiLink}/auction/${auctionId}`, {headers: {'authorization': token}})
+                .then(response => response.json());
+            setAuction(data);
+        }
+        fetchAuction().catch(console.error);
+        ;
+    }, []);
 
 
-    return (
-        <>
-            <div className="auction-details-page">
-                <div className="auction-item">
-                    <div className="upper-container">
-                        <div className="imgContainer"
-                             style={{"background-image": `url("${props.auction.auctionPictureLink}")`}}>
-                            {/*<img className="itemImg" src={props.auctionPictureLink}}/>*/}
-                        </div>
-                        <div className={"profilePart"}>
-                            <div className={"avatarCoint"}>
-                                <img src={props.seller.pictureUrl}/>
-                            </div>
-                            <div className={"profileOne"}>
-                                <h1>{props.seller.firstName}</h1>
-                                <h2>{props.seller.city}</h2>
-                                <h3>{props.seller.postalCode}</h3>
-                            </div>
-                            <div className={"addToCart"}>
-                                <h5>Dodaj do koszyka</h5><span className="material-symbols-outlined">shopping_cart</span>
-                            </div>
-                            <div className={"buyNow"}>
-                                <h5>Napisz wiadomość</h5><span className="material-symbols-outlined">mail</span>
-                            </div>
-                        </div>
-                    </div>
-                    <h1>{props.auction.title}</h1>
-                    <h2>{props.auction.description}</h2>
+    if (!auction) {
+        return (
+            <>
+                <div className="loader-wrapper">
+                    <div className="loader"/>
                 </div>
-            </div>
-            <Footer/>
-        </>
-    );
+            </>
+        )
+    } else {
+        return (
+            <>
+                <div className="auction-details-page">
+                    <div className="auction-item">
+                        <div className="upper-container">
+                            <div className="imgContainer"
+                                 style={{"background-image": `url("${auction.auction.auctionPictureLink}")`}}>
+                                {/*<img className="itemImg" src={props.auctionPictureLink}}/>*/}
+                            </div>
+                            <div className={"profilePart"}>
+                                <div className={"avatarCoint"}>
+                                    <img src={auction.seller.pictureUrl}/>
+                                </div>
+                                <div className={"profileOne"}>
+                                    <h1>{auction.seller.firstName}</h1>
+                                    <h2>{auction.seller.city}</h2>
+                                    <h3>{auction.seller.postalCode}</h3>
+                                </div>
+                                <div className={"addToCart"}>
+                                    <h5>Dodaj do koszyka</h5><span
+                                    className="material-symbols-outlined">shopping_cart</span>
+                                </div>
+                                <div className={"buyNow"}>
+                                    <h5>Napisz wiadomość</h5><span className="material-symbols-outlined">mail</span>
+                                </div>
+                            </div>
+                        </div>
+                        <h1>{auction.auction.title}</h1>
+                        <h2>{auction.auction.description}</h2>
+                    </div>
+                </div>
+                <Footer/>
+            </>
+        );
+    }
+
 };
