d4903a44dc4eb411526b638a10ccee6168dc4753
Adam <al.lo3opole@gmail.com>
2022-11-20 04:43:48 +0100

    Added link to item creation on navbar

--- hackyeah2022/src/components/Navigation/Navigation.jsx
+++ hackyeah2022/src/components/Navigation/Navigation.jsx
@@ -66,6 +66,8 @@ export const Navigation = () => {
                 <div className={window.location.href.match('auction') ? 'underline' : 'no_underline'}><Link to="/user/">Auctions</Link></div>
                 <div className={window.location.href.match('my-items') ? 'underline' : 'no_underline'}><Link to="/user/my-items">My items</Link></div>
                 <div className={window.location.href.match('settings') ? 'underline' : 'no_underline'}><Link to="/user/settings">Settings</Link></div>
+                <div className={window.location.href.match('new-item') ? 'underline' : 'no_underline'}><Link to="/user/new-item">Add item</Link></div>
+
             </nav>
         )
     } else {
@@ -75,6 +77,7 @@ export const Navigation = () => {
                 <div className={window.location.href.match('auction') ? 'underline' : 'no_underline'}><Link to="/user/auction">Auctions</Link></div>
                 <div className={window.location.href.match('my-items') ? 'underline' : 'no_underline'}><Link to="/user/my-items">My items</Link></div>
                 <div className={window.location.href.match('settings') ? 'underline' : 'no_underline'}><Link to="/user/settings">Settings</Link></div>
+                <div className={window.location.href.match('new-item') ? 'underline' : 'no_underline'}><Link to="/user/new-item">Add item</Link></div>
             </nav>
         )
     }   
