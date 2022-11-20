2e2e5a14bc00b8b547eca417468a62b2cf007c47
Kisiekx <43110486+Kisiekx@users.noreply.github.com>
2022-11-20 10:19:10 +0100

    dashboard update

--- hackyeah2022/src/components/App/Dashboard/Dashboard.jsx
+++ hackyeah2022/src/components/App/Dashboard/Dashboard.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
-import { useAuth } from '../../Authentication/AuthProvider'
+import {useAuth} from '../../Authentication/AuthProvider'
+import logo from './../../../assets/recycler.png'
+import './Dashboard.css'
 
 export default function Dashboard() {
 
-    const { token } = useAuth();
+
+    const {token} = useAuth();
 
     return (
         <>
-            <h2>Dashboard</h2>
-            <h1>Token: {token}</h1>
+
+            <div className="dashboard-page">
+                <div style={{"background-image": `url("${logo}")`}} className="logoWrapper">
+
+                </div>
+                <div clssName="search-wrapper">
+                    <span className="material-symbols-outlined">search</span>
+                    <input type="search" placeholder="Search"/>
+                </div>
+            </div>
         </>
     );
 }
